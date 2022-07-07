c3435331621db814d30dc5cf31e1e401e2d4c97a
Irvan Naufali Rahmanto <42972909+irvanrahmanto@users.noreply.github.com>
2022-07-07 16:56:02 +0700

    [7 July 2022] Added new exercises about react for props, life-cycle

--- Front-end/ReactJs/my-app/src/components/class-component/BookAssignment.js
+++ Front-end/ReactJs/my-app/src/components/class-component/BookAssignment.js
@@ -1,92 +1,60 @@
 import React from "react";
-import { library } from './data.js';
+import "./ClassComponent.css";
+import { library } from "./data.js";
 
 class BookAssignment extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            data: false,
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      yourdata: false,
+    };
+  }
 
-    onHanldeClick = () => {
-        if(this.state.data){
-            this.setState({
-                data: false,
-            })
-        }else{
-            this.setState({
-                data: true,
-            })
-        }
-    }
+  onHanldeClick = () => {
+    // if(this.state.yourdata){
+    //     this.setState({
+    //         data: false,
+    //     })
+    // }else{
+    //     this.setState({
+    //         // data: true,
+    //         yourdata: !this.state.yourdata
+    //     })
+    // }
+    this.setState({
+      yourdata: !this.state.yourdata,
+    });
+  };
 
-    render(){
-        return(
-            <div>
-                <button onClick={this.onHanldeClick}>Clik me please</button>
-
-                <div>
-                        <table style={{border:"5px black solid"}}>
-                        <tr>
-                            <th>Id</th>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Categories</th>
-                            <th>Description</th>
-                        </tr>
-                        {library.map((myData =>
-                             <tr>
-                             <td>{myData.id}</td>
-                             <td>{myData.title}</td>
-                             <td>{myData.author}</td>
-                             <td>{myData.categories}</td>
-                             <td>{myData.description}</td>
-                         </tr>
-                        ))}
-
-                    </table>
-                        
-                    </div> 
-            
-                {/* {library.map(myData => (
-                    <p>{myData.author}</p>
-                ))} */}
-
-                {/* {
-                   this.state.myData ?
-                    <div>
-                        <table style={{border:"5px black solid"}}>
-                        <tr>
-                            <th>Id</th>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Categories</th>
-                            <th>Description</th>
-                        </tr>
-                        {library.map((myData =>
-                             <tr>
-                             <td>{myData.id}</td>
-                             <td>{myData.title}</td>
-                             <td>{myData.author}</td>
-                             <td>{myData.categories}</td>
-                             <td>{myData.description}</td>
-                         </tr>
-                        ))}
-
-                    </table>
-                        
-                    </div> 
-                     :  <div 
-                     style={{border:"5px black solid"}}>
-                        <h1>No Data.</h1>
-                     </div> 
-                 
-                } */}
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onHanldeClick}>Clik me please</button>
+        {this.state.yourdata ? (
+          <div>
+            <table table className="BookAssignment">
+              <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Categories</th>
+                <th>Description</th>
+              </tr>
+              {library.map((myData) => (
+                <tr>
+                  <td>{myData.id}</td>
+                  <td>{myData.title}</td>
+                  <td>{myData.author}</td>
+                  <td>{myData.categories}</td>
+                  <td>{myData.description}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default BookAssignment;
--- Front-end/ReactJs/my-app/src/components/class-component/ClassComponent.js
+++ Front-end/ReactJs/my-app/src/components/class-component/ClassComponent.js
@@ -5,21 +5,42 @@ import React from "react";
 import '../class-component/ClassComponent.css';
 
 // Class component
-class ClassComponent extends React.Component{
-    constructor() {
-        super()
+class ClassComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
     }
+
+    // This is life-cycle method!
+    componentDidMount() {
+        this.setState({
+            counter: 1000
+        })
+        // alert('Child component is mounted!')
+    }
+
+    handleChildButton = () => {
+        this.props.childFunction();
+    }
+
+    
+
 
     // Every method component, we should have render! in every render must have return
     render() {
-        return(
-            <div div style = {
-                {
-                    backgroundColor: "red"
-                }
-            } >
-                <h1 className="heading"> This is my class component </h1>
-                <h1></h1>
+        console.log(this.props.toggleStateValue)
+        console.log("My counter value is : ",this.state.counter)
+        return ( 
+            <div> 
+            {
+                this.props.toggleStateValue ?
+                <div className = "div-container" >
+                Halo Irvan!
+                </div> : null
+            } 
+                <button onClick = {this.handleChildButton} > Child Component </button> 
             </div>
         )
     }
